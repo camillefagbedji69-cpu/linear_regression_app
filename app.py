d44d6a6a5c4babaccf49c732edf5b5736669f28d
camillefagbedji69-cpu <camillefagbedji69@gmail.com>
2025-09-09 23:01:04 +0200

    app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ st.title("🌱 Analyse écologique interactive")
 # Upload de fichier
 uploaded_file = st.file_uploader("📂 Chargez un fichier CSV", type="csv")
 
-    if uploaded_file is not None:
-    sep = st.radio("Séparateur", options=[",", ";", "\t"], index=1)
-    df = pd.read_csv(uploaded_file, sep=sep)
-    st.write("Aperçu des données :", df.head())
+    if uploaded_file :
+        sep = st.radio("Séparateur", options=[",", ";", "\t"], index=1)
+        df = pd.read_csv(uploaded_file, sep=sep)
+        st.write("Aperçu des données :", df.head())
 
     # Sélection des colonnes
     colonnes = df.columns.tolist()
@@ -29,6 +29,7 @@ uploaded_file = st.file_uploader("📂 Chargez un fichier CSV", type="csv")
     model = sm.OLS(df[y_col], X).fit()
     st.write("📊 Résultats de la régression :")
     st.write(model.summary())
+
 
 
 
